02c67c428a82c7c34f2ef2b5bbfe12fc09aaa89d
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-07-14 04:27:43 -0400

    Update hacknet.js

--- hacknet.js
+++ hacknet.js
@@ -4,7 +4,7 @@ window.onload=function(){ //cmd window execution here for visuals
     console.group("Information");console.warn("This game was orginially created by Matt Trobbiani, @Orann, please buy the orginial game for the best experience: (https://store.steampowered.com/app/365450/Hacknet/)"),console.warn("This project was created by Bartender (https://steamcommunity.com/id/WineBartender/), a complete recreation of hacknet in javascript for support of all broswers and devices; allowing people to play on their phones anywhere."),console.warn("This game is offine page accessible! You can simply download and play anywhere, even without wifi! (https://github.com/BartenderWinery/Hacknet)"); console.groupEnd()
     if(!appdata.get("settings")){
         appdata.set("settings","")}
-    GUI.insertAdjacentHTML("afterbegin","<iframe src='content/web/Login.html'style='height: 55%;width: 448px;margin-top: 10.8%;;margin-left: 10%;margin-right: 10%'></iframe>")}
+    GUI.insertAdjacentHTML("afterbegin","<iframe src=content/web/Login.html'style='height:55%;width:448px;margin-top:135px;margin-left:180px;margin-right:10%></iframe>")}
 commands=[
     ["help [PAGE NUMBER]"],
     ["scp [filename] [OPTIONAL: destination]"],
