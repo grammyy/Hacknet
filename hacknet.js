af6aa811d4cf175c41462d3777488c0368d85537
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-07-22 04:51:06 -0400

    cookies fixed lol

--- hacknet.js
+++ hacknet.js
@@ -7,7 +7,7 @@ app={
     buffer:120}
 SYS["clear"]=function(e){for(var i=0;i<line;i++)e.children[0].remove();line=1}
 window.onload=function(){ //cmd window execution here for visuals
-    setInterval(function(){GUI.children["taskbar"].children[3].innerText=fps},1)
+    setInterval(function(){GUI.children[1].children[3].innerText=fps},1)
     console.group("Information");console.warn("This game was orginially created by Matt Trobbiani, @Orann, please buy the orginial game for the best experience: (https://store.steampowered.com/app/365450/Hacknet/)"),console.warn("This project was created by Bartender (https://steamcommunity.com/id/WineBartender/), a complete recreation of hacknet in javascript for support of all broswers and devices; allowing people to play on their phones anywhere."),console.warn("This game is offine page accessible! You can simply download and play anywhere, even without wifi! (https://github.com/BartenderWinery/Hacknet)"); console.groupEnd()
     if(!Cookies.get("settings")){
         Cookies.set("settings","")}
@@ -44,6 +44,10 @@ addEventListener("resize",(e)=>{
 //    ["shell"],
 //    ["save!(SJN!*SNL8vAewew57WewJdwl89(*4;;;&!)@&(ak'^&#@J3KH@!*"]]
 API={ //scripts for common buttons and such
+    encode:function(d,s,r){
+        a=JSON.parse(Cookies.get(d))
+        a[s]=r
+        Cookies.set(d,a)},
     toggle:function(data){
         a=data.classList.toggle("1")
         if(a) data.style.backgroundColor="#00c4ff"; else data.style.backgroundColor="#393156d9"},
@@ -77,20 +81,20 @@ profiles={
         GUI.children[4].style.display="flex"
         GUI.children[5].style.display="block"
         if(!String(Cookies.get("accounts")).includes(account)){
-            if(Cookies.get("accounts")&&!Cookies.get(account)){Cookies.set("accounts","{\"data\":\""+JSON.parse(Cookies.get("accounts"))["data"]+";"+account+"\",\"login\":\""+account+"\"}")}
+            if(Cookies.get("accounts")&&!Cookies.get(account)){Cookies.set("accounts","{\"data\":\""+Cookies.parse("accounts")["data"]+";"+account+"\",\"login\":\""+account+"\"}")}
             if(!Cookies.get("accounts")){Cookies.set("accounts","{\"data\":\""+account+"\",\"login\":\""+account+"\"}")}
             if(!Cookies.get(account)){
                 Cookies.set(account,"{\"ip\":\""+(Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))+"\"}")}}
-        app["env"]=JSON.parse(Cookies.get(account))["ip"]+"@>"
+        app["env"]=Cookies.parse(account)["ip"]+"@>"
         app["terminal"].children[0].focus({preventScroll:true})
-        GUI.children["taskbar"].children["net"].children[1].innerText="Home IP: "+JSON.parse(Cookies.get(account))["ip"]
+        GUI.children["taskbar"].children["net"].children[1].innerText="Home IP: "+Cookies.parse(account)["ip"]
         app.cmd.parentElement.childNodes[0].textContent=app.env
         CMD=new Map([
             ["help",["SYS.compile(['?:\>[]',::,''])",["'"+"-".repeat(innerWidth/18.8)+"'","'Command list - Page 1 of 3'"]]],
             ["clear",["SYS.clear(app['terminal'].parentElement)"]],
             ["",["SYS.compile(['"+app.env+"'])"]]])
         CMD.set("help",["SYS.compile(['"+app.env+"[]',::,''])",["'"+"-".repeat(innerWidth/18.8)+"'","'Command list - Page 1 of 3'"]])
-        Cookies.encode("accounts","login",account)},
+        API.encode("accounts","login",account)},
     deload:function(){
         GUI.children[0].style.display="flex"
         GUI.children[1].style.display="none"
@@ -111,16 +115,11 @@ refreshLoop()
 var _cookies=document.cookie?Object.fromEntries(decodeURIComponent(document.cookie).split("; ").map((m)=>{m=m.split("=");return[m[0],m[1]]})):{}
 var Cookies={
     set:function(k,v){
-        _cookies[k]=v
-        this.update()},
-    get:function(k){return _cookies[k]},
+        document.cookie=k+"="+encodeURIComponent(typeof(v)!="object"?v:JSON.stringify(v))
+        return _cookies[k]=v},
+    get:function(k){
+        return _cookies[k]},
+    parse:function(k){
+        return JSON.parse(this.get(k))},
     remove:function(k){
-        delete _cookies[k]
-        this.update()},
-    encode:function(a,b,r){
-        var g=JSON.parse(this.get("app"))
-        g[a][b]=r
-        this.set("app",JSON.stringify(g))},    
-    update:function(){
-        document.cookie.split(";").forEach(function(c){document.cookie=c.replace(/^ +/,"").replace(/=.*/,"=;expires="+new Date().toUTCString()+";path=/")});
-        try{for(var i=0;i<keys(_cookies).length;i++){document.cookie+=keys(_cookies)[i]+"="+_cookies[keys(_cookies)[i]]+"; "}}catch(e){}}}
+        }}
