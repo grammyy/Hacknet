bdb34ba744dbb4c6e1a37ef74ba48e84ea824737
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-07-21 22:04:16 -0400

    fixed cookie system <3

--- hacknet.js
+++ hacknet.js
@@ -68,9 +68,9 @@ render={ //load and deload scenes effectively
     deload:function(){
         document.body.children["SUB"].style.pointerEvents="none"
         document.body.children["SUB"].src=""
-        GUI.style.visibility="visible"},
-    node:function(n,i,x,y){
-        GUI.children[4].insertAdjacentHTML("beforeEnd","<div id='"+i+"' name='"+n+"' class='node' style='left:"+x+"'px;top:'"+y+"px'></div>")}}
+        GUI.style.visibility="visible"}}
+hacknet={node:function(n,i,x,y){
+    GUI.children[4].insertAdjacentHTML("beforeEnd","<div id='"+i+"' name='"+n+"' class='node' style=left:"+x+"%;top:"+y+"%><img src=content/NodeCircle.png style=height:90%></img></div>")}}
 profiles={
     load:function(account){
         document.body.style=document.body.style+""
@@ -91,7 +91,7 @@ profiles={
         app.cmd.parentElement.childNodes[0].textContent=app.env
         CMD=new Map([
             ["help",["SYS.compile(['?:\>[]',::,''])",["'"+"-".repeat(innerWidth/18.8)+"'","'Command list - Page 1 of 3'"]]],
-            ["cls",["SYS.clear(app['terminal'].parentElement)"]],
+            ["clear",["SYS.clear(app['terminal'].parentElement)"]],
             ["",["SYS.compile(['"+app.env+"'])"]]])
         CMD.set("help",["SYS.compile(['"+app.env+"[]',::,''])",["'"+"-".repeat(innerWidth/18.8)+"'","'Command list - Page 1 of 3'"]])
         API.encode("accounts","login",account)},
@@ -111,51 +111,9 @@ function refreshLoop(){
         times.push(now)
         fps = times.length
         refreshLoop()})}
-refreshLoop();
-! function(rcon) {
-    var cook;
-    if ("function" == typeof define && define.amd && (define(rcon), cook = !0), "object" == typeof exports && (module.exports = rcon(), cook = !0), !cook) {
-       var tray = window.Cookies,
-          oven = window.Cookies = rcon();
-       oven.noConflict = function() {
-          return window.Cookies = tray, oven}}}
-(function() {
-    function rcon() {
-        for (var cook = 0, cook = {}; rcon < arguments.length; rcon++) {
-           var tray = arguments[rcon];
-           for (var oven in tray) cook[oven] = tray[oven]}
-        return cook}
-    function cook(rcon) {
-        return rcon.replace(/(%[0-9A-Z]{2})+/g, decodeURIComponent)}
-    return function tray(oven) {
-    function record() {}
-    function write(cook, tray, write) {
-        if ("undefined" != typeof document) {
-            "number" == typeof(write = rcon({
-                path: "/"},
-            record.defaults, write)).expires && (write.expires = new Date(1 * new Date + 864e5 * write.expires)), write.expires = write.expires ? write.expires.toUTCString() : "";
-            try {
-                var read = JSON.stringify(tray);
-                /^[\{\[]/.test(read) && (tray = read)} catch (rcon) {}
-            tray = oven.write ? oven.write(tray, cook) : encodeURIComponent(String(tray)).replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent), cook = encodeURIComponent(String(cook)).replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent).replace(/[\(\)]/g, escape);
-            var ID = "";
-            for (var strings in write) write[strings] && (ID += "; " + strings, !0 !== write[strings] && (ID += "=" + write[strings].split(";")[0]));
-            return document.cookie = cook + "=" + tray + ID}}
-    function read(rcon, tray) {
-        if ("undefined" != typeof document) {
-            for (var record = {}, write = document.cookie ? document.cookie.split("; ") : [], read = 0; read < write.length; read++) {
-                var ID = write[read].split("="),
-                strings = ID.slice(1).join("=");
-                tray || '"' !== strings.charAt(0) || (strings = strings.slice(1, -1));
-                try {
-                    var baked = cook(ID[0]);
-                    if (strings = (oven.read || oven)(strings, baked) || cook(strings), tray) 
-                    try {
-                        strings = JSON.parse(strings)} catch (rcon) {}
-                    if (record[baked] = strings, rcon === baked) break} catch (rcon) {}}
-          return rcon ? record[rcon] : record}}
-    return record.set = write, record.get = function(rcon) {
-        return read(rcon, !1)}, record.getJSON = function(rcon) {
-        return read(rcon, !0)}, record.remove = function(cook, tray) {
-        write(cook, "", rcon(tray, {
-            expires: -1}))}, record.defaults = {}, record.withConverter = tray, record}(function() {})})
+refreshLoop()
+var _cookies=Object.fromEntries(decodeURIComponent(document.cookie).split("; ").map((m)=>{m=m.split("=");return[m[0],m[1]]}))
+var Cookies={
+    set:function(k,v){return _cookies[k]=v},
+    get:function(k){return _cookies[k]},
+    remove:function(k){return delete _cookies["CONSENT"]}}
