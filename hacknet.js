7a19580cabc8845e34e41b68d28a44239754c1e7
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-07-21 23:30:56 -0400

    Update hacknet.js

--- hacknet.js
+++ hacknet.js
@@ -112,8 +112,15 @@ function refreshLoop(){
         fps = times.length
         refreshLoop()})}
 refreshLoop()
-var _cookies=Object.fromEntries(decodeURIComponent(document.cookie).split("; ").map((m)=>{m=m.split("=");return[m[0],m[1]]}))
+var _cookies=document.cookie?Object.fromEntries(decodeURIComponent(document.cookie).split("; ").map((m)=>{m=m.split("=");return[m[0],m[1]]})):{}
 var Cookies={
-    set:function(k,v){return _cookies[k]=v},
+    set:function(k,v){
+        _cookies[k]=v
+        this.update()},
     get:function(k){return _cookies[k]},
-    remove:function(k){return delete _cookies["CONSENT"]}}
+    remove:function(k){
+        delete _cookies[k]
+        this.update()},
+    update:function(){
+        document.cookie.split(";").forEach(function(c){document.cookie=c.replace(/^ +/,"").replace(/=.*/,"=;expires="+new Date().toUTCString()+";path=/")});
+        for(var i=0;i<keys(_cookies).length;i++){document.cookie+=keys(_cookies)[i]+"="+_cookies[keys(_cookies)[i]]+"; "}}}
